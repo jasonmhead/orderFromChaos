567e97cd9df38eef171d257ab49064cf199865a8
jmhead <jmhead@jasonmhead.com>
2015-08-13 13:45:10 -0700

    update to current

---
+++ README.md
@@ -0,0 +1,3 @@
+# orderFromChaos
+A basic rudimentary random word formation experiment
+A working version can be seen at http://jasonmhead.com/orderFromChaos
--- js/main.js
+++ js/main.js
@@ -1,3 +1,46 @@
 function formWord(){
-    var word = $('#wordFld').val();
+    $('.word-btn').text('Working...');
+
+    var word = $('.wordFld').val();
+    word = word.toLowerCase();
+    var wordlen = word.length;
+    
+    //reset
+    $('.final-result').html('');
+    $('.words-gened').html('');
+    
+    var possible = "abcdefghijklmnopqrstuvwxyz";
+    var count = 0;
+    var limit = 50001; // make sure it has a stopping point
+    var genWord = '';
+    
+    while(word != genWord && count < limit){
+        genWord = makeword(wordlen);  
+        count++;
+        
+        if(word == genWord){
+            $('.final-result').html('It took '+count+' tries to make the word "'+word+'"');
+            $('.words-gened').prepend("<b>"+genWord+"</b>, ");
+            $('.word-btn').text('Form Word Randomly');
+        }else{
+            $('.words-gened').prepend(genWord+", ");
+        }
+        
+        if(count == limit-1){
+            $('.final-result').html(count+' tries and still no match for "'+word+'"');
+            $('.word-btn').text('Form Word Randomly');
+        }
+    }
+        
+}
+
+function makeword(wordlen)
+{
+    var text = "";
+    var possible = "abcdefghijklmnopqrstuvwxyz";
+
+    for( var i=0; i < wordlen; i++ )
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
 }
